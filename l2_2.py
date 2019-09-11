3cd20a4fed5ba6fd2db8c78e4112db9e00364c78
vivekangadi1@github.com <vivekangadi1@github.com>
2019-09-12 03:00:16 +0530

    adding the missing code'

--- l2_2.py
+++ l2_2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 X = []
 Y = []
 
-for line in open('data_2d.csv'):
+for line in open('inear_regression_formulation\data_2d.csv'):
     x1,x2,y = line.split(',')
     X.append([float(x1),float(x2),1])
     Y.append(float(y))
@@ -22,3 +22,13 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(X[:,0], X[:,1], Y)
 plt.show()
+
+
+w = np.linalg.solve(np.dot(X.T,X),np.dot(X.T,Y))
+
+Yhat = np.dot(X,w)
+
+d1 = Y - Yhat
+d2 = Y - Y.mean()
+r2 = 1 - d1.dot(d1) / d2.dot(d2)
+print("the r-squared is:", r2)
