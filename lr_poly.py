f178341de2663c60d175828cd41e150e797c22ff
vivekangadi1@github.com <vivekangadi1@github.com>
2019-09-13 03:18:14 +0530

    minor changes

--- lr_poly.py
+++ lr_poly.py
@@ -22,17 +22,16 @@ w = np.linalg.solve(np.dot(X.T,X),np.dot(X.T,Y))
 
 
 plt.scatter(X[:,1], Y)
-x_line = np.linspace(X[:,1].min(), X[:,1].max())
-y_line = w[0] + w[1] * x_line + w[2] * x_line * x_line
-plt.plot(x_line, y_line)
-plt.title("Our fitted quadratic")
-plt.show()
-
 Yhat = X.dot(w)
+
+#plotting the fitted line
+plt.scatter(X[:,1],Yhat)
+plt.title('predicted data')
+plt.show()
 
 
 d1 = Y - Yhat
 d2 = Y - Y.mean()
 
-r2 = 1 - d1.dot(d1)/d2.dot(d2)
+r = 1 - d1.dot(d1)/d2.dot(d2)
 print("the r2 value",r2)
